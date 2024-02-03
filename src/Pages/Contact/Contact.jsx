34be698a08592056607ade932f121ae4c774843f
MuhammadAtiq757 <muhammadatiq757@gmail.com>
2024-02-03 23:58:25 +0600

    contact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -18,14 +18,16 @@ const handleSuccess=()=>{
     return (
         <div className=' mx-auto pt-[60px] meet'>
             
-    <div className=''>
-     <h1 className='text-center font-semibold text-2xl text-white font-serif'>CONTACT US</h1>
+    
+  <div>
+  <h1 className='text-center font-semibold text-2xl text-white font-serif'>CONTACT US</h1>
    <p className='border-b-2 border-black w-[30px] mx-auto mt-6'></p>
      <p className='text-center mt-10 text-white'>WE launch our new website soon. <br />
      Please stay update and follow
      </p>
+  </div>
 
-     <div className='flex justify-center items-center h-screen '>
+     <div className='flex justify-center items-center h-screen form'>
   <div className=''>
     <input type="text" placeholder="Name" className="mt-4 input input-bordered border-black w-[300px] max-w-xs" /> <br />
     <input type="text" placeholder="Email" className="mt-4 input input-bordered border-black w-[300px] max-w-xs" /> <br />
@@ -36,7 +38,7 @@ const handleSuccess=()=>{
 </div>
 
 
-    </div>
+   
 
 
         </div>
